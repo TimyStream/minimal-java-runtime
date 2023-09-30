ad079aa20a89e853f5eaf6c2646fca31d86d59c4
Timy Stream <50535748+TimyStream@users.noreply.github.com>
2023-09-30 22:04:28 +0200

    Added Upload Command for Github Repository

--- build-11.sh
+++ build-11.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker build --rm --compress -f Java11-Dockerfile -t ghcr.io/timystream/mjrt:11 .
+docker push ghcr.io/timystream/mjrt:11
--- build-17.sh
+++ build-17.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker build --rm --compress -f Java17-Dockerfile -t ghcr.io/timystream/mjrt:17 .
+docker push ghcr.io/timystream/mjrt:17
